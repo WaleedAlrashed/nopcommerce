d5d700b621af2d16863996e0f3a8727523ee5476
Developers betters digital <wmr1210@hotmail.com>
2024-06-09 16:35:26 +0400

    Add TabbyPaymentMethod and Settings

---
+++ src/Plugins/Nop.Plugin.Payments.Tabby/TabbyPaymentSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Nop.Plugin.Payments.Tabby
+{
+        public class TabbyPaymentSettings : ISettings
+        {
+            public string PublicKey { get; set; }
+            public string SecretKey { get; set; }
+        }
+}
+
