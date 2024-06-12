707a18d8dd80c3ec627c7d328d6c4770323f94db
Developers betters digital <wmr1210@hotmail.com>
2024-06-12 21:19:55 +0400

    Add comments in PaymentinfoViewComponent

--- src/Plugins/Nop.Plugin.Payments.Tabby/Components/PaymentInfoViewComponent.cs
+++ src/Plugins/Nop.Plugin.Payments.Tabby/Components/PaymentInfoViewComponent.cs
@@ -3,13 +3,27 @@ using Nop.Web.Framework.Components;
 
 namespace Nop.Plugin.Payments.Tabby.Components
 {
+    /// <summary>
+    /// Represents the view component to display payment info in public store
+    /// </summary>
     public class PaymentInfoViewComponent : NopViewComponent
     {
+        #region Methods
+
+        /// <summary>
+        /// Invoke view component
+        /// </summary>
+        /// <param name="widgetZone">Widget zone name</param>
+        /// <param name="additionalData">Additional data</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the view component result
+        /// </returns>
         public async Task<IViewComponentResult> InvokeAsync(string widgetZone, object additionalData)
         {
-            // Add any logic you need to pass data to the view here
 
             return View("~/Plugins/Payments.Tabby/Views/PaymentInfo.cshtml");
         }
+        #endregion
     }
 }
