5130abc3adbafd0618fbfcd9fba05d54f9bc9d37
Developers betters digital <wmr1210@hotmail.com>
2024-06-12 22:24:30 +0400

    Create RouteProvider.cs

---
+++ src/Plugins/Nop.Plugin.Payments.Tabby/Infrastructure/RouteProvider.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Nop.Web.Framework.Mvc.Routing;
+
+namespace Nop.Plugin.Payments.Tabby.Infrastructure;
+
+/// <summary>
+/// Represents plugin route provider
+/// </summary>
+public class RouteProvider : IRouteProvider
+{
+    /// <summary>
+    /// Register routes
+    /// </summary>
+    /// <param name="endpointRouteBuilder">Route builder</param>
+    public void RegisterRoutes(IEndpointRouteBuilder endpointRouteBuilder)
+    {
+        endpointRouteBuilder.MapControllerRoute(TabbyDefaults.ConfigurationRouteName,
+            "Admin/Tabby/Configure",
+            new { controller = "Tabby", action = "Configure" });
+
+        endpointRouteBuilder.MapControllerRoute(TabbyDefaults.WebhookRouteName,
+            "Plugins/Tabby/Webhook",
+            new { controller = "TabbyWebhook", action = "WebhookHandler" });
+    }
+
+    /// <summary>
+    /// Gets a priority of route provider
+    /// </summary>
+    public int Priority => 0;
+}
