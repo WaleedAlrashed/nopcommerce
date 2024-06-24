9155ed69ac8b2bb44a35683c7cb8069ff5b38c3a
Developers betters digital <wmr1210@hotmail.com>
2024-06-24 17:48:09 +0400

    Update TabbyPaymentSettings.cs

--- src/Plugins/Nop.Plugin.Payments.Tabby/TabbyPaymentSettings.cs
+++ src/Plugins/Nop.Plugin.Payments.Tabby/TabbyPaymentSettings.cs
@@ -10,7 +10,7 @@ namespace Nop.Plugin.Payments.Tabby
         /// <summary>
         /// Gets or sets public key
         /// </summary>
-        public string PublicKey { get; set; } = string.Empty;
+        public string PublicKey { get; set; } = "pk_test_80d3109b-b620-4121-bb99-02cb63faef76";
 
         /// <summary>
         /// Gets or sets secret key
